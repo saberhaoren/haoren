e495331ab946bc9c038e0ad44f090a19568356c1
TaoZhengrong <787938381@qq.com>
2024-06-23 14:04:52 +0800

    Quartz sync: Jun 23, 2024, 2:04 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -29,6 +29,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
+    Component.MobileOnly(Component.Explorer()),
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
@@ -45,7 +46,5 @@ export const defaultListPageLayout: PageLayout = {
     Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
   ],
-  right: [
-    Component.MobileOnly(Component.Explorer()),
-  ],
+  right: [],
 }
